454530d513dfc798d2a196b3d99655886903c348
IngsR <ikhwan2ende@gmail.com>
2025-07-21 18:53:08 +0700

    fix layout public

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,9 +3,8 @@ import JumpingDotsLoader from '@/components/ui/jumping-dots-loader';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 
-/**
- * A simple loader component to be used as a fallback for Suspense.
- */
+export const dynamic = 'force-dynamic';
+
 function PageLoader() {
     return (
         <div className="flex min-h-[calc(100vh-200px)] items-center justify-center">
